a5fbb5874b4ef29d972720bc15218f88e237a099
ruaa7 <rwy664175@gmail.com>
2026-09-23 08:56:04 +0300

    Update home.php

--- views/photos/home.php
+++ views/photos/home.php
@@ -1,10 +1,10 @@
 <div class="p-5 mb-4 rounded-4 text-center" >
     <h1 class="display-5 fw-bold" style="color: var(--primary);">Welcome to Alzikrayat</h1>
-    <p class="lead mb-2" style="color: var(--primary-dark);">"Alzikrayat" means <em>memories</em>.Memories make life special</p>
+    <p class="lead mb-2" style="color: var(--primary-dark);">"Alzikrayat" means <em>memories</em>, memories make life special</p>
     <p class="lead mb-4" style="color: var(--primary-dark);"> With Alzikrayat you can share, 
     upload, browse and comment on the special moments that matter to you and your friends.</p>
    <?php if (empty($_SESSION['user_id'])): ?>
-    <a href="<?= url('/register') ?>" class="fs-5">Join Now</a>
+    <a href="<?= url('/register') ?>" class="fs-5 btn btn-primary btn-lg">Join Now!</a>
    <?php else: ?>
     <a href="<?= url('/photo/create') ?>">Upload a Photo</a>
    <?php endif; ?>
