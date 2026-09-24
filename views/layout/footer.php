1d82161d66a40dab49551bfc26abd8b112376f3d
ruaa7 <rwy664175@gmail.com>
2026-09-24 10:09:33 +0300

    Update footer.php

--- views/layout/footer.php
+++ views/layout/footer.php
@@ -2,7 +2,7 @@
 
 <footer class="text-light py-4 mt-5 navbar-custom">
     <div class="container text-center">
-        <p class="mb-1">Alzikrayat &copy; <?= date('Y') ?> — Project 1, Advanced Web Technologies, SUST</p>
+        <p class="mb-1">Alzikrayat &copy; <?= date('Y') ?> - Project 1, Advanced Web Technologies, SUST</p>
         <p class="small text-secondary mb-0">Built from scratch with a custom, lightweight MVC Software framework.</p>
     </div>
 </footer>
